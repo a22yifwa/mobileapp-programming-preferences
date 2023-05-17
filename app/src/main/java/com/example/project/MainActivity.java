147bc7bf6a4f085562be4b15d470e5d3c700e9e7
a22yifwa <a22yifwa@student.his.se>
2023-05-17 15:00:26 +0200

    Skapa en knapp som kan klicka att går till andra sidan

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.project;
-
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText; // <--- this is the import that is required for EditText to work in your code
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import androidx.appcompat.widget.Toolbar;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+
+            }
+
     }
 
-}
