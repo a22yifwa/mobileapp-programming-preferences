7aaea3c3d30f8442561282ed08444c782ed7d924
a22yifwa <a22yifwa@student.his.se>
2023-05-17 15:23:02 +0200

    Add a button in SecondActivity and can go back to the first page

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,14 +1,28 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.widget.EditText; // <--- this is the import that is required for EditText to work in your code
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import androidx.appcompat.widget.Toolbar;
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button button = findViewById(R.id.start_first_activity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
